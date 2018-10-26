a8bcd8b1354b67e4a4fa72a4f15b441846c51730
Alka <alkamailgames@gmail.com>
2018-10-26 20:52:03 +0200

    Scripts edit

--- Script/Apple.gd
+++ Script/Apple.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+onready var players = get_tree().get_nodes_in_group("hero")
+onready var  player = players[0] # To get a Ref to the player
+
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,3 +13,12 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("player"):
+		if body.actualGameplay.size() >= 4:
+			player.actualGameplay += "Apple"
+			queue_free()
+		else:
+			pass
