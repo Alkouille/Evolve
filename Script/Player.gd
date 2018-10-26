df3dbe554b4b3ab7ee04ad0a46ecaf2b1d9bd216
Alka <alkamailgames@gmail.com>
2018-10-26 20:27:28 +0200

    Add Apple Scene and his script

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,15 +1,27 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var actualGameplay = ["moveright"]
+var finalGamePlay = ["moveright", "moveLeft", "moveUpAndDown", "Talk"]
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _physics_process(delta):
+	input()
+
+func input():
+	if actualGameplay.size() >= 1:
+		MoveRight()
+	
+	if actualGameplay.size() >= 2:
+		MoveLeft()
+
+func MoveRight():
+	if Input.is_action_pressed("WalkRight"):
+			move_and_slide(Vector2(150, 0))
+
+func MoveLeft():
+	if Input.is_action_pressed("WalkLeft"):
+		move_and_slide(Vector2(-150, 0))
